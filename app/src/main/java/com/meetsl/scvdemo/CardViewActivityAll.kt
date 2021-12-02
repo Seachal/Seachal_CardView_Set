2a71dc2d21b7943de49b3e32d51e61e8025889fb
seachal <seachalzhang@gmail.com>
2021-12-02 17:26:36 +0800

    添加一个全部案例

---
+++ app/src/main/java/com/meetsl/scvdemo/CardViewActivityAll.kt
@@ -0,0 +1,12 @@
+package com.meetsl.scvdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CardViewActivityAll : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_view_all)
+    }
+}
